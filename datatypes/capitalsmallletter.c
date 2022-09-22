8d4e04c7244e4200a77bf114e178fd397bd0648b
AtharvaKanaki <113452583+AtharvaKanaki@users.noreply.github.com>
2022-09-22 17:44:16 +0530

    Update and rename CAPITALO.C to capitalsmallletter.c

--- datatypes/capitalsmallletter.c
+++ datatypes/capitalsmallletter.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
 char c;
-clrscr();
 printf("Enter a alphabet:\n");
 scanf("%c",&c);
 if(c<=90)
@@ -11,5 +9,4 @@ printf("The alphabet %c is capital.\n",c);
 else
 printf("The alphabet %c is small.\n",c);
 printf("Thank you.\n");
-getch();
 }
