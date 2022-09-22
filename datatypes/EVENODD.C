167248c8c2dcc1c05d83a6cafb414fc801b5071f
AtharvaKanaki <113452583+AtharvaKanaki@users.noreply.github.com>
2022-09-22 17:41:27 +0530

    Update EVENODD.C

--- datatypes/EVENODD.C
+++ datatypes/EVENODD.C
@@ -1,14 +1,11 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
 int a;
-clrscr();
-printf("Enter a number:\n");
-scanf("%d",a);
-if(a%=0)
-printf("The number %d is EVEN number.\n",a);
+printf("Enter a number:");
+scanf("%d",&a);
+if(a%2)
+printf("The number %d is ODD number.\n",a);
 else
-printf("The numver %d is ODD number.\n",a);
-getch();
+printf("The number %d is EVEN number.\n",a);
 }
