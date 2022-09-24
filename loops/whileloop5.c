1a82ae724894010bf0e976c91c2e53348d8dce8e
AtharvaKanaki <113452583+AtharvaKanaki@users.noreply.github.com>
2022-09-24 14:22:00 +0530

    Add files via upload

---
+++ loops/whileloop5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int n,ans;
+    n=1;
+    while(n<15)
+    {
+        ans=7*n;
+        printf(" %d ",ans);
+        ++n;
+    }
+    printf("\nThank you.\n");
+}
