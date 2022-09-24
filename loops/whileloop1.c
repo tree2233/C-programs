5a32ca9fc3fed9a14e7efff019673c3625206f40
AtharvaKanaki <113452583+AtharvaKanaki@users.noreply.github.com>
2022-09-24 14:20:32 +0530

    Add files via upload

---
+++ loops/whileloop1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int i;
+    printf("Numbers from 1 to 10:");
+    i=1;
+    while(i<=10)
+    {
+        printf(" %d ",i);
+        ++i;
+    }
+    printf("\nThank you.\n");
+}
