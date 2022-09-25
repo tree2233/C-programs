0577439a6c71550708d79bfd9903d02f33738748
AtharvaKanaki <113452583+AtharvaKanaki@users.noreply.github.com>
2022-09-26 00:15:40 +0530

    Add files via upload

---
+++ datatypes/ifelseif2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int age;
+    printf("Enter your age:");
+    scanf("%d",&age);
+    if(age>60)
+        printf("You are a Sr. Citizen.\n");
+    else if(age>=18)
+        printf("You are a Adult.\n");
+    else if(age>=12)
+        printf("You are a Teenager.\n");
+    else if(age>=4)
+        printf("You are a Child.\n");
+    else
+        printf("You are a Infant.\n");
+    printf("Thank you.\n");
+}
