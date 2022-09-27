c580ca31dead14ca7d157883991c8b8e21888438
AtharvaKanaki <113452583+AtharvaKanaki@users.noreply.github.com>
2022-09-27 22:16:39 +0530

    Add files via upload

---
+++ datatypes/areaofrectangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int l,b,area;
+    printf("Enter the length and breath of the Rectangle:");
+    scanf("%d %d",&l,&b);
+    area=l*b;
+    printf("Area of the Rectangle is %d square units.\n",area);
+    printf("Thank you.\n");
+}
