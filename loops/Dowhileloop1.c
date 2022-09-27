bd095949ce428bcdeb38c6853d72a7b43403fdfc
AtharvaKanaki <113452583+AtharvaKanaki@users.noreply.github.com>
2022-09-27 22:18:08 +0530

    Add files via upload

---
+++ loops/Dowhileloop1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int no;
+    printf("Numbers from 1 to 10 are :\n");
+    no=1;
+    do
+    {
+        printf(" %d ",no);
+        ++no;
+    }while(no<11);
+    printf("\nThank you.\n");
+}
