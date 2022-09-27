c183bef6ccffb29ff0c22c34e6b6fb2fcc2167b7
AtharvaKanaki <113452583+AtharvaKanaki@users.noreply.github.com>
2022-09-27 19:29:25 +0530

    Add files via upload

---
+++ loops/forloop1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("Numbers from 1 to 10:\n");
+    for(a=1;a<=10;++a)
+    {
+        printf(" %d ",a);
+    }
+    printf("\nThank you.\n");
+}
