f348c56ef584e483c8cf20396bca2b6ceabcb14b
AtharvaKanaki <113452583+AtharvaKanaki@users.noreply.github.com>
2022-09-27 22:16:59 +0530

    Add files via upload

---
+++ datatypes/areaofsquare.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int a,area;
+    printf("Enter the side of Square:");
+    scanf("%d",&a);
+    area=a*a;
+    printf("Area of the Square is %d square units.\n",area);
+    printf("Thank you.\n");
+}
