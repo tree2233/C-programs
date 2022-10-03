2df2f4e626137b988f6fa5e15bfdfaba8eec4caa
AtharvaKanaki <113452583+AtharvaKanaki@users.noreply.github.com>
2022-10-03 23:07:06 +0530

    Add files via upload

---
+++ arrays/Arrays4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int i[5];
+    float f[10];
+    char c[20];
+    printf("The size of integer i = %d bytes.\n",sizeof(i));
+    printf("The size of float f = %d bytes.\n",sizeof(f));
+    printf("The size of char c = %d bytes.\n",sizeof(c));
+    printf("Thank you.\n");
+}
