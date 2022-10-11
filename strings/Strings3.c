1e4cd953d51d9353e2d7b8db28e4e6ac8c7ab4ac
AtharvaKanaki <113452583+AtharvaKanaki@users.noreply.github.com>
2022-10-11 22:51:08 +0530

    Add files via upload

---
+++ strings/Strings3.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+    char name[20]={'A','T','H','A','R','V','A','\0'};
+    char Surname[]={'K','A','N','A','K','I','\0'};
+    printf("Hello %s %s.\n",name,Surname);
+    printf("Thank you.\n");
+}
