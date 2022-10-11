67cef1bc4c207321e3dbcb4085fa3a274d9db6cd
AtharvaKanaki <113452583+AtharvaKanaki@users.noreply.github.com>
2022-10-11 22:50:50 +0530

    Add files via upload

---
+++ strings/Strings2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    char name[20];
+    name[0]='A';
+    name[1]='T';
+    name[2]='H';
+    name[3]='A';
+    name[4]='R';
+    name[5]='V';
+    name[6]='A';
+    name[7]='\0';
+    printf("Hello %s\n",name);
+    printf("Thank you.\n");
+}
