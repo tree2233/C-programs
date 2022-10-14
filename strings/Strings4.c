430d50d1e9c79ca0bdc1fa4107f2617835a9ad84
AtharvaKanaki <113452583+AtharvaKanaki@users.noreply.github.com>
2022-10-14 18:07:31 +0530

    Add files via upload

---
+++ strings/Strings4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    char name[20]="Atharva";
+    char institute[20]="INTELLISE IT";
+    char address[]="369,Sakhar Peth, Solapur";
+    printf("Hello %s\n",name);
+    printf("Your Institute name is %s.\n",institute);
+    printf("Your address is %s.\n",address);
+    printf("Thank you.\n");
+}
