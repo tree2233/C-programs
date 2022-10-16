50d5f76a0bacbd50b2109e322f64f5cdd4a5682d
AtharvaKanaki <113452583+AtharvaKanaki@users.noreply.github.com>
2022-10-16 18:18:13 +0530

    Update ArrayOfStrings.c

--- strings/ArrayOfStrings.c
+++ strings/ArrayOfStrings.c
@@ -3,6 +3,7 @@ void main()
 {
     char strArr[5][20];
     int i;
+    printf("Size of memory allocated to string array \"strArr[5][20]\" = %d bytes.\n", sizeof(strArr));
     printf("Enter 5 names:");
     for(i=0;i<5;++i)
     gets(strArr[i]);
