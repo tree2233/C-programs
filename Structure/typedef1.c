0575479296ad0b7465cc90fb1d621ec8ea6dc78c
AtharvaKanaki <113452583+AtharvaKanaki@users.noreply.github.com>
2022-10-24 13:38:09 +0530

    Add files via upload

---
+++ Structure/typedef1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    typedef int integer;
+    integer i;
+    integer j;
+    printf("Enter 2 numbers:");
+    scanf("%d %d",&i,&j);
+    printf("The numbers entered are,i = %d and j = %d.\n",i,j);
+    printf("Thank you.\n");
+}
